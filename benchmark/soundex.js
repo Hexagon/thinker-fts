ac4b67af96a809506497a49521216b90daa24c49
Hexagon <robinnilsson@gmail.com>
2016-11-12 23:25:30 +0100

    Code style fixes, minor bugfix, release 1.0.10

--- benchmark/soundex.js
+++ benchmark/soundex.js
@@ -1,13 +1,13 @@
 var 
-	Benchmark = require('benchmark'),
-	Thinker = require('../'),
+	Benchmark = require("benchmark"),
+	Thinker = require("../"),
 	suite = new Benchmark.Suite,
 	soundex = Thinker.processors.soundex();
 
-suite.add('Soundex', function() {
-  var result = soundex('convolution');
+suite.add("Soundex", function() {
+  var result = soundex("convolution");
 })
-.on('cycle', function(event) {
+.on("cycle", function(event) {
   console.log(String(event.target));
 })
 .run();
--- benchmark/stemmers.swedish.js
+++ benchmark/stemmers.swedish.js
@@ -1,13 +1,13 @@
 var 
-	Benchmark = require('benchmark'),
-	Thinker = require('../'),
+	Benchmark = require("benchmark"),
+	Thinker = require("../"),
 	suite = new Benchmark.Suite,
 	swedishStemmer = Thinker.processors.stemmers.swedish();
 
-suite.add('Swedish stemmer', function() {
-  var result = swedishStemmer('friserandets');
+suite.add("Swedish stemmer", function() {
+  var result = swedishStemmer("friserandets");
 })
-.on('cycle', function(event) {
+.on("cycle", function(event) {
   console.log(String(event.target));
 })
 .run();
